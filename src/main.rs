0918ca100f8c19248ba4a79a06ca639e141e5309
Noah Kennedy <nkennedy@cloudflare.com>
2022-04-14 10:38:27 -0500

    minify

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,8 @@
 #[tokio::main(flavor = "current_thread")]
 async fn main() {
-    tracing_subscriber::fmt().init();
     tokio::spawn(do_request()).await.unwrap();
 }
 
 async fn do_request() {
-    let response = reqwest::get("https://google.com").await.unwrap();
-    tracing::info!("{}", response.text().await.unwrap());
+    tracing::info!("{}", std::future::ready("test").await);
 }
